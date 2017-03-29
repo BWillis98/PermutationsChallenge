9d94f9bba2237938234423b63bdaaafeef9c5e90
Brandon Willis <bwmusiclover1212@gmail.com>
2017-03-29 00:11:30 -0500

    Code in stable state, but logical errors

--- src/permutation/Handler.java
+++ src/permutation/Handler.java
@@ -1,5 +1,6 @@
 package permutation;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Handler {
@@ -7,35 +8,80 @@ public class Handler {
 	String input;
 	
 	char[] inputCharArray;
+	
 	int[] indexesOfInputStringAsCharArray;
 	
 	Scanner sc = new Scanner(System.in);
 	
-	void begin(){
+	private void begin(){
 		System.out.println("Enter in the string you want to permutate\n");
 		input = sc.next();
+		
 		convertInputToArray();	
 		process(indexesOfInputStringAsCharArray);
 	}
 
-
-	
-	void process(int[] input){
+	private void process(int[] input){
 		
-		printInput();
+		printInputParameter();
 		
+		// Searches for a time where a number is bigger than the one that's to its left.
 		for (int i = input.length - 1; i > 0; i--){
 			if (input[i] > input[i - 1]){
-				for (int x = 0; x < (i - 1); x++){
-					
-				}
+				updateIndexesArray(i - 1);
+				process(indexesOfInputStringAsCharArray);
 			}
 		}
+		
 		return;
 		
 	}
 	
-	public void convertInputToArray() {
+	private void updateIndexesArray(int indexOfMajorChangingNumber) {
+		// Shorter variable names
+		int majorIndex = indexOfMajorChangingNumber;
+		int majorNumber = indexesOfInputStringAsCharArray[majorIndex];
+		
+		// Guaranteed for there to be a next smallest b/c current method only gets called if there is.
+		swapWithNextSmallestNumber(majorIndex, majorNumber);
+		
+		sortRemainder(majorIndex);
+	}
+	
+	// I know this is inefficient, but its temporary
+	public void sortRemainder(int majorIndex) {
+		
+		int remainderLength = (indexesOfInputStringAsCharArray.length - 1) - majorIndex;
+		int[] leftOverArrayToSort = new int[remainderLength];
+		
+		System.out.println("Remainder Length = " + remainderLength);
+		for (int i = majorIndex + 1; i < indexesOfInputStringAsCharArray.length - 1; i++){
+			leftOverArrayToSort[i] = indexesOfInputStringAsCharArray[i];
+		}
+		Arrays.sort(leftOverArrayToSort);
+		for (int i = majorIndex + 1; i < indexesOfInputStringAsCharArray.length - 1; i++){
+			 indexesOfInputStringAsCharArray[i] = leftOverArrayToSort[i];
+		}
+	}
+
+	public void swapWithNextSmallestNumber(int majorIndex, int majorNumber) {
+		
+		boolean done = false;
+		int nextBiggest = majorNumber;
+		
+		while (!done){
+			nextBiggest++;
+			for (int i = majorIndex + 1; i < indexesOfInputStringAsCharArray.length; i++){
+				if (indexesOfInputStringAsCharArray[i] == nextBiggest){
+					indexesOfInputStringAsCharArray[majorIndex] = indexesOfInputStringAsCharArray[i];
+					indexesOfInputStringAsCharArray[i] = majorNumber;
+					done = true;
+				}
+			}
+		}
+	}
+
+	private void convertInputToArray() {
 		inputCharArray = input.toCharArray();
 		indexesOfInputStringAsCharArray = new int[inputCharArray.length];
 		for (int i = 0; i < inputCharArray.length; i++){
@@ -43,14 +89,17 @@ public class Handler {
 		}
 	}
 	
-	public void printInput() {
+	
+	private void printInputParameter() {
+		System.out.println();
 		String output = "";
 		for (int index : indexesOfInputStringAsCharArray){
 			output += inputCharArray[index];
-			System.out.println(index);
+			System.out.print(index);
 		}
-		System.out.println(output);
+		System.out.println("\n" + output);
 	}
+	
 
 	public static void main(String[] args) {
 		Handler handler = new Handler();
